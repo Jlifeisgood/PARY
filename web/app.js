48b6a3c48a9ebe6d55ca5a5f6d44d3149fe12fa6
Jlifeisgood <115610251+Jlifeisgood@users.noreply.github.com>
2026-09-26 10:23:09 +0500

    deploy web

--- web/app.js
+++ web/app.js
@@ -188,7 +188,7 @@ const LANGS_SHORT = [["uz", "UZ"], ["ru", "RU"], ["en", "EN"]];
 // ------------------------------------------------------------------ заставка
 
 const APP_NAME = "Пары";
-const FLIP_STEP = 0.32, FLIP_DUR = 0.95;
+const FLIP_STEP = 0.3, FLIP_DUR = 1.15;
 function startSplash() {
   // календарь, у которого страницы перелистываются к сегодняшнему дню
   const now = nowT(), mon = T.months[now.getUTCMonth()].slice(0, 3).toUpperCase();
